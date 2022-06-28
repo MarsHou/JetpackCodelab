70108e2ee962835ae15c7c7274bd22d15e4a3b6c
Mars <houjianchao1204@gmail.com>
2022-06-28 10:31:39 +0800

    status up level

--- app/src/main/java/com/mars/jetpacklearn/MainActivity.kt
+++ app/src/main/java/com/mars/jetpacklearn/MainActivity.kt
@@ -3,16 +3,10 @@ package com.mars.jetpacklearn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,8 +24,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")) {
+fun MyApp() {
+
+    var shouldShowOnBoarding by remember {
+        mutableStateOf(true)
+    }
+    if (shouldShowOnBoarding) {
+        OnBoardingScreen {
+            shouldShowOnBoarding = false
+        }
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(names: List<String> = listOf("World", "Compose")) {
     Column(modifier = Modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
@@ -64,6 +74,24 @@ fun Greeting(name: String) {
         }
     }
 }
+
+@Composable
+fun OnBoardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to the Basics Codelab!")
+            Button(modifier = Modifier.padding(24.dp),
+                onClick = { onContinueClicked() }) {
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
