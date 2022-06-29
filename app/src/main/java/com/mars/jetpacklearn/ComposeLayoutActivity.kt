81c5b2e286e70a58cf36292a11db9a9d2d066b03
Mars <houjianchao1204@gmail.com>
2022-06-29 17:12:13 +0800

    create customize layout

--- app/src/main/java/com/mars/jetpacklearn/ComposeLayoutActivity.kt
+++ app/src/main/java/com/mars/jetpacklearn/ComposeLayoutActivity.kt
@@ -21,8 +21,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.mars.jetpacklearn.ui.theme.JetpackLearnTheme
@@ -37,10 +42,50 @@ class ComposeLayoutActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackLearnTheme {
-                SimpleList()
+                MyBodyContent()
             }
         }
     }
+
+    @Composable
+    fun MyBodyContent(modifier: Modifier = Modifier) {
+        MyOwnColumn(modifier.padding(8.dp)) {
+            Text("MyOwnColumn")
+            Text("places items")
+            Text("vertically.")
+            Text("We've done it by hand!")
+        }
+    }
+
+    @Composable
+    fun MyOwnColumn(modifier: Modifier, content: @Composable () -> Unit) {
+        Layout(modifier = modifier, content = content) { measurables, constrains ->
+            val placeables = measurables.map {
+                it.measure(constrains)
+            }
+
+            var yPosition = 0
+            layout(constrains.maxWidth, constrains.maxHeight) {
+                placeables.forEach { placeable ->
+                    placeable.placeRelative(x = 0, y = yPosition)
+                    yPosition += placeable.height
+                }
+            }
+        }
+    }
+
+    private fun Modifier.firstBaselineToTop(firstBaseLineToTop: Dp) =
+        this.then(layout() { measurable, constraints ->
+            val placeable = measurable.measure(constraints)
+            check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+            val firstBaseLine = placeable[FirstBaseline]
+
+            val placeableY = firstBaseLineToTop.roundToPx() - firstBaseLine
+            val height = placeable.height + placeableY
+            layout(placeable.width, height) {
+                placeable.placeRelative(0, placeableY)
+            }
+        })
 
 
     @Composable
@@ -150,7 +195,23 @@ class ComposeLayoutActivity : ComponentActivity() {
     @Composable
     fun PhotographerCardPreview() {
         JetpackLearnTheme {
-            SimpleList()
+            MyBodyContent()
+        }
+    }
+
+    @Preview
+    @Composable
+    fun TextWithPaddingToBaselinePreview() {
+        JetpackLearnTheme {
+            Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+        }
+    }
+
+    @Preview
+    @Composable
+    fun TextWithNormalPaddingPreview() {
+        JetpackLearnTheme {
+            Text("Hi there!", Modifier.padding(top = 32.dp))
         }
     }
 }
