eb0873dacc0c05398415c10f0b1676234c6f28ce
Mars <houjianchao1204@gmail.com>
2022-06-29 15:06:25 +0800

    use list

--- app/src/main/java/com/mars/jetpacklearn/ComposeLayoutActivity.kt
+++ app/src/main/java/com/mars/jetpacklearn/ComposeLayoutActivity.kt
@@ -3,12 +3,12 @@ package com.mars.jetpacklearn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -17,13 +17,16 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.mars.jetpacklearn.ui.theme.JetpackLearnTheme
+import kotlinx.coroutines.launch
 
 /**
  * Created by Mars on 2022/6/29 09:47.
@@ -34,11 +37,57 @@ class ComposeLayoutActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackLearnTheme {
-                LayoutsCodelab()
+                SimpleList()
             }
         }
     }
 
+
+    @Composable
+    fun SimpleList() {
+        val listSize = 100
+        val scrollState = rememberLazyListState()
+
+        val coroutineScope = rememberCoroutineScope()
+        Column() {
+            Row() {
+                Button(onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(0)
+                    }
+                }) {
+                    Text(text = "Scroll to the top")
+                }
+
+                Button(onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(listSize - 1)
+                    }
+                }) {
+                    Text("Scroll to the end")
+                }
+            }
+            LazyColumn(state = scrollState) {
+                items(listSize) {
+                    ImageListItem(it)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun ImageListItem(index: Int) {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+                contentDescription = "Android Logo",
+                modifier = Modifier.size(50.dp)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
+        }
+
+    }
 
     @Composable
     fun LayoutsCodelab() {
@@ -101,7 +150,7 @@ class ComposeLayoutActivity : ComponentActivity() {
     @Composable
     fun PhotographerCardPreview() {
         JetpackLearnTheme {
-            LayoutsCodelab()
+            SimpleList()
         }
     }
 }
