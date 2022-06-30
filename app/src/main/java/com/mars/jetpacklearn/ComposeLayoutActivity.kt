a9394f454005f29fc5df58a178d4e493978b840a
Mars <houjianchao1204@gmail.com>
2022-06-30 15:57:31 +0800

    codelab constraint layout

--- app/src/main/java/com/mars/jetpacklearn/ComposeLayoutActivity.kt
+++ app/src/main/java/com/mars/jetpacklearn/ComposeLayoutActivity.kt
@@ -20,14 +20,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.AlignmentLine
-import androidx.compose.ui.layout.FirstBaseline
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.layoutId
 import coil.compose.rememberImagePainter
 import com.mars.jetpacklearn.ui.theme.JetpackLearnTheme
 import kotlinx.coroutines.launch
@@ -41,8 +42,78 @@ class ComposeLayoutActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackLearnTheme {
-                ChipBodyContent()
+                DecoupledConstraintLayout()
             }
+        }
+    }
+
+    @Composable
+    fun DecoupledConstraintLayout() {
+        BoxWithConstraints {
+            val constraints = if (maxWidth < maxHeight) {
+                decoupledConstraints(margin = 16.dp) // Portrait constraints
+            } else {
+                decoupledConstraints(margin = 32.dp) // Landscape constraints
+            }
+
+            ConstraintLayout(constraints) {
+                Button(onClick = { /*TODO*/ }, modifier = Modifier.layoutId("button")) {
+                    Text(text = "Button")
+                }
+
+                Text(text = "Text", modifier = Modifier.layoutId("text"))
+            }
+        }
+    }
+
+    private fun decoupledConstraints(margin: Dp): ConstraintSet {
+        return ConstraintSet {
+            val button = createRefFor("button")
+            val text = createRefFor("text")
+
+            constrain(button) {
+                top.linkTo(parent.top, margin)
+            }
+            constrain(text) {
+                top.linkTo(button.bottom, margin)
+                centerHorizontallyTo(button)
+            }
+        }
+    }
+
+    @Composable
+    fun ConstraintLayoutContent() {
+        ConstraintLayout {
+            val (button1, button2, text1, text2) = createRefs()
+
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.constrainAs(button1) {
+                top.linkTo(parent.top, margin = 16.dp)
+            }) {
+                Text(text = "Button1")
+            }
+
+            Text(text = "text", modifier = Modifier.constrainAs(text1) {
+                top.linkTo(button1.bottom, margin = 16.dp)
+                centerAround(button1.end)
+            })
+
+            val barrier = createEndBarrier(button1, text1)
+
+            Button(onClick = {}, modifier = Modifier.constrainAs(button2) {
+                top.linkTo(parent.top, 16.dp)
+                start.linkTo(barrier)
+            }) {
+                Text(text = "Button2")
+            }
+
+            val guideline = createGuidelineFromStart(0.5f)
+
+            Text(
+                text = "This is a very very very very very very very long text",
+                modifier = Modifier.constrainAs(text2) {
+                    linkTo(start = guideline, end = parent.end)
+                    width = Dimension.preferredWrapContent
+                })
         }
     }
 
@@ -273,7 +344,7 @@ class ComposeLayoutActivity : ComponentActivity() {
     @Composable
     fun PhotographerCardPreview() {
         JetpackLearnTheme {
-            ChipBodyContent()
+            DecoupledConstraintLayout()
         }
     }
 
